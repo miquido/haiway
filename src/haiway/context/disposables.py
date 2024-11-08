09d5019797260de98ff03055f462fc89a80eb1e1
Kacper Kaliński <kacper.kalinski@miquido.com>
2024-11-08 16:03:45 +0100

    Rework disposables interface

--- src/haiway/context/disposables.py
+++ src/haiway/context/disposables.py
@@ -1,20 +1,19 @@
-from asyncio import gather, shield
+from asyncio import gather
 from collections.abc import Iterable
+from contextlib import AbstractAsyncContextManager
+from itertools import chain
 from types import TracebackType
-from typing import Protocol, final, runtime_checkable
+from typing import final
 
 from haiway.state import State
+from haiway.utils import freeze
 
 __all__ = [
     "Disposable",
     "Disposables",
 ]
 
-
-@runtime_checkable
-class Disposable(Protocol):
-    async def initialize(self) -> State | None: ...
-    async def dispose(self) -> None: ...
+type Disposable = AbstractAsyncContextManager[Iterable[State] | State | None]
 
 
 @final
@@ -25,39 +24,35 @@ class Disposables:
     ) -> None:
         self._disposables: tuple[Disposable, ...] = disposables
 
-    async def initialize(self) -> Iterable[State]:
-        return [
-            state
-            for state in await gather(
-                *[disposable.initialize() for disposable in self._disposables],
-            )
-            if state is not None
-        ]
-
-    async def dispose(self) -> None:
-        results: list[BaseException | None] = await shield(
-            gather(
-                *[disposable.dispose() for disposable in self._disposables],
-                return_exceptions=True,
-            ),
-        )
-
-        self._disposables = ()
-        exceptions: list[BaseException] = [
-            exception for exception in results if exception is not None
-        ]
-
-        if len(exceptions) > 1:
-            raise BaseExceptionGroup("Disposing errors", exceptions)
-
-        elif exceptions:
-            raise exceptions[0]
+        freeze(self)
 
     def __bool__(self) -> bool:
         return len(self._disposables) > 0
 
+    async def _initialize(
+        self,
+        disposable: Disposable,
+        /,
+    ) -> Iterable[State]:
+        match await disposable.__aenter__():
+            case None:
+                return ()
+
+            case State() as single:
+                return (single,)
+
+            case multiple:
+                return multiple
+
     async def __aenter__(self) -> Iterable[State]:
-        return await self.initialize()
+        return [
+            *chain.from_iterable(
+                state
+                for state in await gather(
+                    *[self._initialize(disposable) for disposable in self._disposables],
+                )
+            )
+        ]
 
     async def __aexit__(
         self,
@@ -65,4 +60,19 @@ class Disposables:
         exc_val: BaseException | None,
         exc_tb: TracebackType | None,
     ) -> None:
-        await self.dispose()
+        results: list[bool | BaseException | None] = await gather(
+            *[
+                disposable.__aexit__(
+                    exc_type,
+                    exc_val,
+                    exc_tb,
+                )
+                for disposable in self._disposables
+            ],
+            return_exceptions=True,
+        )
+
+        exceptions: list[BaseException] = [exc for exc in results if isinstance(exc, BaseException)]
+
+        if len(exceptions) > 1:
+            raise BaseExceptionGroup("Disposing errors", exceptions)
