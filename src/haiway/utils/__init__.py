08853fc3d1e66141007947d5d6cfc1b3b29bbefc
Kacper Kaliński <kacper.kalinski@miquido.com>
2025-01-10 10:31:37 +0100

    Add builtin collection types conversion

--- src/haiway/utils/__init__.py
+++ src/haiway/utils/__init__.py
@@ -2,13 +2,18 @@ from haiway.utils.always import always, async_always
 from haiway.utils.env import getenv_bool, getenv_float, getenv_int, getenv_str, load_env
 from haiway.utils.immutable import freeze
 from haiway.utils.logs import setup_logging
+from haiway.utils.mappings import as_dict
 from haiway.utils.mimic import mimic_function
 from haiway.utils.noop import async_noop, noop
 from haiway.utils.queue import AsyncQueue
+from haiway.utils.sequences import as_list, as_tuple
 
 __all__ = [
     "AsyncQueue",
     "always",
+    "as_dict",
+    "as_list",
+    "as_tuple",
     "async_always",
     "async_noop",
     "freeze",
