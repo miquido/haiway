2a5906903eb2aff8ce5691930a78b01a992a7d67
Kacper Kaliński <kacper.kalinski@miquido.com>
2025-03-12 16:39:41 +0100

    Add label to traced wrapper

--- src/haiway/helpers/tracing.py
+++ src/haiway/helpers/tracing.py
@@ -1,6 +1,6 @@
 from asyncio import iscoroutinefunction
 from collections.abc import Callable, Coroutine, Mapping, Sequence
-from typing import Any, Self, cast
+from typing import Any, Self, cast, overload
 
 from haiway.context import ctx
 from haiway.state import State
@@ -61,28 +61,53 @@ class ResultTrace(State):
     result: Any | Missing
 
 
+@overload
 def traced[**Args, Result](
     function: Callable[Args, Result],
     /,
-) -> Callable[Args, Result]:
-    if __debug__:
-        if iscoroutinefunction(function):
-            return cast(
-                Callable[Args, Result],
-                _traced_async(
-                    function,
-                    label=function.__name__,
-                ),
-            )
+) -> Callable[Args, Result]: ...
 
-        else:
-            return _traced_sync(
-                function,
-                label=function.__name__,
-            )
 
-    else:  # do not trace on non debug runs
-        return function
+@overload
+def traced[**Args, Result](
+    *,
+    label: str,
+) -> Callable[[Callable[Args, Result]], Callable[Args, Result]]: ...
+
+
+def traced[**Args, Result](
+    function: Callable[Args, Result] | None = None,
+    /,
+    *,
+    label: str | None = None,
+) -> Callable[[Callable[Args, Result]], Callable[Args, Result]] | Callable[Args, Result]:
+    def wrap(
+        wrapped: Callable[Args, Result],
+    ) -> Callable[Args, Result]:
+        if __debug__:
+            if iscoroutinefunction(wrapped):
+                return cast(
+                    Callable[Args, Result],
+                    _traced_async(
+                        wrapped,
+                        label=label or wrapped.__name__,
+                    ),
+                )
+
+            else:
+                return _traced_sync(
+                    wrapped,
+                    label=label or wrapped.__name__,
+                )
+
+        else:  # do not trace on non debug runs
+            return wrapped
+
+    if function := function:
+        return wrap(wrapped=function)
+
+    else:
+        return wrap
 
 
 def _traced_sync[**Args, Result](
